c90ffa31b8e302cc0d82e6ee636dc815e5a54827
rg33-dev <raghavsankhyaan@gmail.com>
2026-07-02 17:28:28 +0530

    service and controller

--- src/main/java/com/example/crud/controller/studentController.kt
+++ src/main/java/com/example/crud/controller/studentController.kt
@@ -1,41 +1,79 @@
 package com.example.crud.controller
 
 import com.example.crud.entity.Student
+import com.example.crud.repository.StudentRepo
 import com.example.crud.service.StudentService
-
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.*
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+
 
 @Component//THis is optional to use in this case e since we're already using @RestController
 @RestController
 @RequestMapping("/api/student")
-class StudentController(  private val studentService: StudentService   ){ //constructor DI unlike java
+class StudentController(
+    private val studentService: StudentService, private val studentRepo: StudentRepo
+)//constructor DI unlike java
 
 
-
+{
 
 
     @PostMapping("/add")
-    fun createStudent(@RequestBody  student: Student) : Student {
-       return  studentService.create(student)
+    fun createStudent(@RequestBody student: Student): Student {
+        return studentService.create(student)
 
     }
 
-    fun updateStudent(student: Student): Student {
-        return student
+    @GetMapping("/get/{id}")    //here we cant hardcode id like id=1, cause it'll fetch constant values
+    //so use path variable here using @Pathvaraible
+    fun getStudent(@PathVariable id: Long, student: Student): ResponseEntity<Student> {
+        val studentResponse = studentRepo.findById(id)
 
+        return if (studentResponse.isPresent) {
+            ok(studentResponse.get())
+        } else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+
+        }
     }
 
-    fun deleteStudent(student: Student): Student {
-        return student
-    }
-     @GetMapping("/all")
-    fun readStudent(student: Student): Student {
-        return student
+    @GetMapping
+    fun getAllStudents(): ResponseEntity<List<Student>> {
+        val students = studentService.getAllStudent()
+
+        return if (students.isEmpty()) {
+            ResponseEntity.noContent().build()
+        } else {
+            ResponseEntity.ok(students)
+        }
     }
 
+    @PutMapping("/{id}")
+    fun updateStudent(
+        @PathVariable id: Long, @RequestBody studentReq: Student
+    ): ResponseEntity<Student> = studentService.updateStudent(id, studentReq)?.let {
+        ResponseEntity.ok(it)
+    } ?: ResponseEntity.notFound().build()
+
+    @DeleteMapping("/{id}")
+    fun deleteStudent(@PathVariable id: Long): ResponseEntity<Void> = if (studentService.deleteStudent(id)) {
+        ResponseEntity.noContent().build()
+    } else {
+        ResponseEntity.notFound().build()
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/example/crud/service/studentservice.kt
+++ src/main/java/com/example/crud/service/studentservice.kt
@@ -15,6 +15,33 @@ class StudentService(private val repo : StudentRepo) {
         return requestByUser
 
     }
+
+
+    fun getAllStudent(): List<Student> {
+        return repo.findAll()
+    }
+
+    fun updateStudent(id: Long, studentReq: Student): Student? {
+        val existingStudent = repo.findById(id).orElse(null)
+            ?: return null
+
+        existingStudent.name = studentReq.name
+        existingStudent.id = id
+        existingStudent.subject = studentReq.subject
+        existingStudent.mail = studentReq.mail
+        existingStudent.age = studentReq.age
+
+        return repo.save(existingStudent)
+    }
+
+    fun deleteStudent(id: Long): Boolean {
+        if (!repo.existsById(id)) {
+            return false
+        }
+
+            repo.deleteById(id)
+        return true
+    }
 }
 
 
